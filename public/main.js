9e2f30c0e6ccd18e37b514823610edf104466476
Aaron Cates <aaron.cates@aaroncates-ltm.internal.salesforce.com>
2018-04-12 11:43:24 -0700

    changes

--- public/main.js
+++ public/main.js
@@ -96,8 +96,28 @@ var BlockSDK = __webpack_require__(1);
 	  var imagealt = $(this).attr("alt");
 	  var imageurl = $(this).attr("src");
 		console.log("Alt Text: " + imagealt + "|| Image Source: " + imageurl);
-  	sdk.setContent(imageurl);
-  	sdk.setSuperContent('This is super content: ' + imagealt)
+	});
+
+	sdk.getContent(function (content) {
+
+		function saveText() {
+			sdk.setContent(imagealt);
+			sdk.setSuperContent('This is super content: ' + imageurl);
+
+			sdk.getData(function (data) {
+				var numberOfEdits = data.numberOfEdits || 0;
+				sdk.setData({
+					numberOfEdits: numberOfEdits + 1
+				});
+			});
+
+			sdk.getCentralData(function (central) {
+				var totalNumberOfEdits = central.totalNumberOfEdits || 0;
+				sdk.setCentralData({
+					totalNumberOfEdits: totalNumberOfEdits + 1
+				});
+			});
+		}
 	});
 
 	//function saveText() {
