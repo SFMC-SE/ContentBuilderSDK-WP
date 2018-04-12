5312eab86b23f6a69e65900a0f5f53435fdffc89
Aaron Cates <aaron.cates@aaroncates-ltm.internal.salesforce.com>
2018-04-12 11:23:13 -0700

    changes

--- public/main.js
+++ public/main.js
@@ -72,36 +72,39 @@ var BlockSDK = __webpack_require__(1);
 //if (window.self === window.top) {
 //	document.body.innerText = 'This application is for use in the Salesforce Marketing Cloud Content Builder Editor only.';
 //} else {
+
 	var sdk = new BlockSDK();
 
-	sdk.getContent(function (content) {
-		var quill = new Quill('#editor-container', {
-			theme: 'snow'
-		});
-		quill.root.innerHTML = content;
+	var $btns = $('.slds-button_neutral').click(function() {
+	// if all is selected show all elements in main div - by default the page loads with all values selected
+	if (this.id == 'all') {
+	    $('#cms-images > img').show();
+	} else {
+	    // take id from selected button and create var with class value same as id, hide those elements that don't have that class
+	    var $el = $('.' + this.id).show();
+	    $('#cms-images > img').not($el).hide();
+	}
+	// remove active class from all buttons
+	$btns.removeClass('active');
+	// add active class to selected button
+	$(this).addClass('active');
+	})
+	var imageurl;
+	var imagealt;
 
-		function saveText() {
-			var html = quill.root.innerHTML;
-			sdk.setContent(html);
-			sdk.setSuperContent('This is super content: ' + html);
-
-			sdk.getData(function (data) {
-				var numberOfEdits = data.numberOfEdits || 0;
-				sdk.setData({
-					numberOfEdits: numberOfEdits + 1
-				});
-			});
-
-			sdk.getCentralData(function (central) {
-				var totalNumberOfEdits = central.totalNumberOfEdits || 0;
-				sdk.setCentralData({
-					totalNumberOfEdits: totalNumberOfEdits + 1
-				});
-			});
-		}
-
-		quill.on('text-change', saveText);
+	$("img").click(function(){
+	  var imagealt = $(this).attr("alt");
+	  var imageurl = $(this).attr("src");
+		console.log("Alt Text: " + imagealt + "|| Image Source: " + imageurl);
+  	sdk.setContent(imageurl);
+  	sdk.setSuperContent('This is super content: ' + imagealt)
 	});
+
+	//function saveText() {
+	//  sdk.setContent(imageurl);
+	//  sdk.setSuperContent('This is super content: ' + imagealt)
+	//}
+
 //}
 
 
