acef799dd9a1fc1546ddf85c4647eb7499bfae51
Aaron Cates <aaron.cates@aaroncates-ltm.internal.salesforce.com>
2018-04-25 18:32:28 -0500

    changes

--- public/main.js
+++ public/main.js
@@ -85,6 +85,16 @@ document.getElementById('block-container').addEventListener("input", function ()
 	sliderValues();
 });
 
+document.getElementById('block-container').addEventListener("click", function () {
+	debounce(setImage, 500)();
+	sliderValues();
+});
+
+document.getElementById('block-container').addEventListener("blur", function () {
+	debounce(setImage, 500)();
+	sliderValues();
+});
+
 //document.getElementById('slider-image-width').addEventListener("input", function () {
 //	debounce(setImage, 500)();
 //	sliderValues();
