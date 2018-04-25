8b7ee8e1961f8a1045be060014b8d72a15039e9d
Aaron Cates <aaron.cates@aaroncates-ltm.internal.salesforce.com>
2018-04-25 10:13:34 -0500

    changes

--- public/main.js
+++ public/main.js
@@ -1,51 +1,126 @@
-	var $btns = $('.slds-button_neutral').click(function() {
-	// if all is selected show all elements in main div - by default the page loads with all values selected
-	if (this.id == 'all') {
-	    $('#cms-images > img').show();
-	} else {
-	    // take id from selected button and create var with class value same as id, hide those elements that don't have that class
-	    var $el = $('.' + this.id).show();
-	    $('#cms-images > img').not($el).hide();
-	}
-	// remove active class from all buttons
-	$btns.removeClass('active');
-	// add active class to selected button
-	$(this).addClass('active');
-	})
-	var imageurl;
-	var imagealt;
 
-	$("img").click(function(){
-	  var imagealt = $(this).attr("alt");
-	  var imageurl = $(this).attr("src");
-		console.log("Alt Text: " + imagealt + "|| Image Source: " + imageurl);
-	});
+var imageurl, imagealt;
 
-	sdk.getContent(function (content) {
+// SDK
+var sdk = new window.sfdc.BlockSDK();
 
-		function saveText() {
-			sdk.setContent(imagealt);
-			sdk.setSuperContent('This is super content: ' + imageurl);
+var link, width, height, imagealt, imageurl;
 
-			sdk.getData(function (data) {
-				var numberOfEdits = data.numberOfEdits || 0;
-				sdk.setData({
-					numberOfEdits: numberOfEdits + 1
-				});
-			});
+function debounce (func, wait, immediate) {
+	var timeout;
+	return function() {
+		var context = this, args = arguments;
+		var later = function() {
+			timeout = null;
+			if (!immediate) func.apply(context, args);
+		};
+		var callNow = immediate && !timeout;
+		clearTimeout(timeout);
+		timeout = setTimeout(later, wait);
+		if (callNow) func.apply(context, args);
+	};
+}
 
-			sdk.getCentralData(function (central) {
-				var totalNumberOfEdits = central.totalNumberOfEdits || 0;
-				sdk.setCentralData({
-					totalNumberOfEdits: totalNumberOfEdits + 1
-				});
-			});
-		}
-	});
+// look into setting selected state for image and embedding in setImage function
+$("img").click(function() {
+	var imageurl = $(this).attr("src");
+	var imagealt = $(this).attr("alt");
+})
 
-	//function saveText() {
-	//  sdk.setContent(imageurl);
-	//  sdk.setSuperContent('This is super content: ' + imagealt)
-	//}
+function blockSettings () {
+	document.getElementById('image-link').value = link;
+	document.getElementById('slider-image-width').value = width;
+	document.getElementById('slider-image-height').value = height;
+}
 
-//}
+function sliderValues () {
+	document.getElementById('slider-image-width-val').innerHTML = document.getElementById('slider-image-width').value;
+	document.getElementById('slider-image-height-val').innerHTML = document.getElementById('slider-image-height').value;
+}
+
+function setImage() {
+	link = document.getElementById('image-link').value;
+	width = document.getElementById('slider-image-width').value;
+	height = document.getElementById('slider-image-height').value;
+//	imageurl = document.getElementById('');
+//	imageurl = "avatar1.jpg";
+	//set function to define image as active class
+//	$("img").click(function(){
+//	  var imagealt = $(this).attr("alt");
+//	  var imageurl = $(this).attr("src");
+	  // set input from image-link as href of selected image
+//	  var link = $('#image-link').val();
+//	  var height= $('#slider-image-height').val();
+//	  var width= $('#slider-image-width').val();
+
+	//  var link = "www.google.com";
+//	  console.log("Alt Text: " + imagealt + "|| Image Source: " + imageurl);
+//	  console.log("height" + height + "|| width " + width);
+	//  sdk.setContent(imagealt);
+	//  sdk.setSuperContent('This is super content: ' + imageurl);
+	  sdk.setContent('<a href="' + link + '"><img height="' + height + '" width="' + width + '" src="https://experts-cb-sdk-wordpress.herokuapp.com/' + imageurl + '" /></a>');
+		sdk.setData({
+			link: link,
+			width: width,
+			height: height,
+			imageurl: imageurl
+		});
+}
+
+sdk.getData(function (data) {
+	link = data.link || '';
+	width = data.width || 300;
+	height = data.height || 300;
+	link = data.link || '';
+	blockSettings();
+	sliderValues();
+	setImage();
+});
+// SLIDERS
+// Update the displayed value of the sliders
+//$("#slider-image-width").change(function() {
+//  $('#slider-image-width-val').text($(this).val());
+//});
+//$("#slider-image-height").change(function() {
+//  $('#slider-image-height-val').text($(this).val());
+//});
+
+// BUTTONS
+// filter results based on buttons selected
+var $btns = $('.slds-button_neutral').click(function() {
+// if all is selected show all elements in main div - by default the page loads with all values selected
+if (this.id == 'all') {
+    $('#cms-images > img').show();
+} else {
+    // take id from selected button and create var with class value same as id, hide those elements that don't have that class
+    var $el = $('.' + this.id).show();
+    $('#cms-images > img').not($el).hide();
+}
+// remove active class from all buttons
+$btns.removeClass('active');
+// add active class to selected button
+$(this).addClass('active');
+})
+
+// old code to set SDK values
+//$("img").click(function(){
+//  var imagealt = $(this).attr("alt");
+//  var imageurl = $(this).attr("src");
+  // set input from image-link as href of selected image
+//  var link, height, width;
+//  var link = $('#image-link').val();
+//  var height= $('#slider-image-height').val();
+//  var width= $('#slider-image-width').val();
+
+//  var link = "www.google.com";
+//  console.log("Alt Text: " + imagealt + "|| Image Source: " + imageurl);
+//  console.log("height" + height + "|| width " + width);
+//  sdk.setContent(imagealt);
+//  sdk.setSuperContent('This is super content: ' + imageurl);
+//  sdk.setContent('<a href="' + link + '"><img height="' + height + '" width="' + width + '" src="https://experts-cb-sdk-wordpress.herokuapp.com/' + imageurl + '" /></a>');
+//});
+
+document.getElementById('blockcontainer').addEventListener("input", function () {
+	debounce(setImage, 500)();
+	sliderValues();
+});
