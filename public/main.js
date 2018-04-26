74d878f95c0dcece9c831cde930505418ca51793
Aaron Cates <aaron.cates@aaroncates-ltm.internal.salesforce.com>
2018-04-25 22:37:02 -0500

    changes

--- public/main.js
+++ public/main.js
@@ -3,20 +3,73 @@
 // get image data from data.json
 //$.getJSON("images.json", function(data) {
 //		var returnedImages = '';
-		// loop through each value to dynamically build html from json data values and load to tiles var
+		// loop through each value to dynamically build html from json data values and build image elements
 //		$.each(data, function(key, value) {
-//				returnedImages += '<img class="slds-p-around_xxx-small ' + value.tag + '" src="' + value.link + '" alt="' + value.name + '" width="100" height="100">';
- //		});
+//				returnedImages += '<img class="slds-p-around_xxx-small ' + value.tag + '" src="' + value.location + '" alt="' + value.name + '" width="100" height="100">';
+//		});
 		// append html generated to cms-images div
 //		$('#cms-images').html(returnedImages);
 //});
 
-// append an id to the selected image and remove id from any other selected images
-//$('.slds-p-around_xxx-small').click(function() {
-$('img').click(function() {
-	$('img').removeAttr('id');
-	$(this).attr('id', 'selected-image');
-})
+var debounce = _.debounce(setImage, 300);
+
+// SDK
+var sdk = new window.sfdc.BlockSDK();
+
+var link, width, height, imagealt, imageurl;
+
+//function debounce (func, wait, immediate) {
+//	var timeout;
+//	return function() {
+//		var context = this, args = arguments;
+//		var later = function() {
+//			timeout = null;
+//			if (!immediate) func.apply(context, args);
+//		};
+//		var callNow = immediate && !timeout;
+//		clearTimeout(timeout);
+//		timeout = setTimeout(later, wait);
+//		if (callNow) func.apply(context, args);
+//	};
+//}
+
+function blockSettings () {
+//    document.getElementsByTagName("img").src(imageurl).setAttribute("id", "selected-image");
+	document.getElementById('image-link').value = link;
+	document.getElementById('slider-image-width').value = width;
+	document.getElementById('slider-image-height').value = height;
+}
+
+function sliderValues () {
+	document.getElementById('slider-image-width-val').innerHTML = document.getElementById('slider-image-width').value;
+	document.getElementById('slider-image-height-val').innerHTML = document.getElementById('slider-image-height').value;
+}
+
+function setImage() {
+	link = document.getElementById('image-link').value;
+	width = document.getElementById('slider-image-width').value;
+	height = document.getElementById('slider-image-height').value;
+		sdk.setContent('<a href="' + link + '"><img height="' + height + '" width="' + width + '" src="' + imageurl + '" /></a>');
+		sdk.setData({
+			link: link,
+			width: width,
+			height: height,
+			imageurl: imageurl
+		});
+}
+
+sdk.getData(function (data) {
+	link = data.link || '';
+	width = data.width || 300;
+	height = data.height || 300;
+	imageurl = data.imageurl || '';
+    blockSettings();
+	sliderValues();
+    
+});
+
+
+
 
 // BUTTONS
 // filter results based on buttons selected
@@ -36,84 +89,41 @@ $(this).addClass('active');
 })
 
 
-
-// SDK
-var sdk = new window.sfdc.BlockSDK();
-
-var link, width, height, imagealt, imageurl;
-
-function debounce (func, wait, immediate) {
-	var timeout;
-	return function() {
-		var context = this, args = arguments;
-		var later = function() {
-			timeout = null;
-			if (!immediate) func.apply(context, args);
-		};
-		var callNow = immediate && !timeout;
-		clearTimeout(timeout);
-		timeout = setTimeout(later, wait);
-		if (callNow) func.apply(context, args);
-	};
-}
-
-function blockSettings () {
-	document.getElementById('image-link').value = link;
-	document.getElementById('slider-image-width').value = width;
-	document.getElementById('slider-image-height').value = height;
-	document.getElementById('selected-image').src = imageurl;
-}
-
-function sliderValues () {
-	document.getElementById('slider-image-width-val').innerHTML = document.getElementById('slider-image-width').value;
-	document.getElementById('slider-image-height-val').innerHTML = document.getElementById('slider-image-height').value;
-}
-
-function setImage() {
-	link = document.getElementById('image-link').value;
-	width = document.getElementById('slider-image-width').value;
-	height = document.getElementById('slider-image-height').value;
-	imageurl = document.getElementById('selected-image').src;
-		sdk.setContent('<a href="' + link + '"><img height="' + height + '" width="' + width + '" src="' + imageurl + '" /></a>');
-		sdk.setData({
-			link: link,
-			width: width,
-			height: height,
-			imageurl: imageurl
-		});
-}
-
-sdk.getData(function (data) {
-	link = data.link || '';
-	width = data.width || 300;
-	height = data.height || 300;
-	imageurl = data.imageurl || '';
-	blockSettings();
-	sliderValues();
-	setImage();
-});
-
 // EVENT LISTENERS
 
-document.getElementById('slider-image-width').addEventListener("input", function () {
-	debounce(setImage, 500)();
-	sliderValues();
-});
 
-document.getElementById('slider-image-height').addEventListener("input", function () {
-	debounce(setImage, 500)();
-	sliderValues();
-});
+// set image url after user click
 
-document.getElementById('image-link').addEventListener("blur", function () {
-	debounce(setImage, 500)();
-	sliderValues();
-});
+$("#cms-images").children("img").click(function() {
+//	$("img").removeAttr('id');
+//	$(this).attr('id', 'selected-image');
+    imageurl = $(this).attr('src');
+//    debounce(setImage, 500)();
+    debounce();
+})
 
-document.getElementById('cms-images').addEventListener("click", function () {
-	debounce(setImage, 500)();
-	sliderValues();
-});
+$("#slider-image-width, #slider-image-height, #image-link").change(function() {
+    debounce();
+    sliderValues();
+})
+
+//document.getElementById('slider-image-width').addEventListener("input", function () {
+//	debounce();
+//	sliderValues();
+//});
+
+//document.getElementById('slider-image-height').addEventListener("input", function () {
+//	debounce();
+//	sliderValues();
+//});
+
+//document.getElementById('image-link').addEventListener("blur", function () {
+//	debounce();
+//});
+
+//document.getElementById('cms-images').addEventListener("click", function () {
+//	debounce(setImage, 500)();
+//});
 
 
 // BUTTONS
