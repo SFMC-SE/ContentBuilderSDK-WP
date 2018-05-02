c55b238ce84b95709d60e4353aa35367546a98e0
Aaron Cates <aaron.cates@aaroncates-ltm.internal.salesforce.com>
2018-05-02 16:36:47 -0500

    changes

--- public/main.js
+++ public/main.js
@@ -50,7 +50,7 @@ function setImage() {
 	link = document.getElementById('image-link').value;
 	width = document.getElementById('slider-image-width').value;
 	height = document.getElementById('slider-image-height').value;
-    sdk.setContent('<a href="' + link + '"><img height="' + height + '" width="' + width + '" src="' + imageurl + '" /></a>');
+    sdk.setContent('<a href="' + link + '"><img align = "center" height="' + height + '" width="' + width + '" src="' + imageurl + '" /></a>');
 	sdk.setData({
 	link: link,
 	width: width,
