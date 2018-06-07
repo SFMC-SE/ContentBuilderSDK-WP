96eb60d7f71da8008c67ea80730743dcad3e8196
William Garrison <wgarrison@salesforce.com>
2018-06-07 14:18:35 -0700

    whatever

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 var express = require('express');
+var path = require('path');
+var request = require('request');
 var app = express();
 
 app.set('port', (process.env.PORT || 3000));
 
 app.use(express.static(__dirname + '/public'));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
 app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+ 	console.log('Node app is running on port', app.get('port'));
 });
+
+app.get('/', function(req, res){
+	res.render('pages/index');
+}); 
